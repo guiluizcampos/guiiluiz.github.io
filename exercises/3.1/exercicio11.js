3788b75494a7e3a8a6aa496d5f7b8451c805ab6b
Guilherme Luiz <guiiluiz44@gmail.com>
2019-09-23 19:30:45 -0300

    adiciona estrutura de condicao if para calcular o imposto de renda baseado no salario inserido

--- exercises/3.1/exercicio11.js
+++ exercises/3.1/exercicio11.js
@@ -12,3 +12,15 @@ if(salBruto < 1556.95) {
 }else{
     inss = 570.88;
 }
+
+if(salBruto < 1903.99) {
+    ir = 0;
+}else if(salBruto < 2826,66) {
+    ir = 7.5/100 * salBruto - 142.80;
+}else if(salBruto < 3751,06) {
+    ir = 15/100 * salBruto - 354.80;
+}else if(salBruto < 4664,69) {
+    ir = 22.5/100 * salBruto - 636.13;
+}else{
+    ir = 27.5/100 * salBruto - 869.36;
+}

316c5cba5bc1e18a5dd410a7790f0705286c5b68
Guilherme Luiz <guiiluiz44@gmail.com>
2019-09-23 19:30:05 -0300

    adiciona estrutura de cndicao if para calcular o inss baseado no salario inserido

--- exercises/3.1/exercicio11.js
+++ exercises/3.1/exercicio11.js
@@ -2,3 +2,13 @@ let salBruto = 1000;
 let salLiq;
 let inss;
 let ir;
+
+if(salBruto < 1556.95) {
+    inss = 8/100 * salBruto;
+}else if(salBruto < 2594.93) {
+    inss = 9/100 * salBruto;
+}else if(salBruto < 5189.83) {
+    inss = 11/100 * salBruto;
+}else{
+    inss = 570.88;
+}

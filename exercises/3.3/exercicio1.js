1f589eb8b838529d8e8e6b9854ded54e67f251d6
Guilherme Luiz <guiiluiz44@gmail.com>
2019-09-25 19:24:53 -0300

    cria e resolve exercicio1

---
+++ exercises/3.3/exercicio1.js
@@ -0,0 +1,20 @@
+function verificaPalindrome(palindrome) {
+    let entrada = "o galo ama o lago";
+    
+    //remove espaços em branco
+    let esp; 
+    esp = entrada.replace( /\s/g, '' );
+
+    //separa entrada por string
+    var separado = esp.split("");
+
+    let resultado = true;
+    let i;
+    
+    for (i = 0; i < separado.length; i++) {
+        if (separado[i] != separado[separado.length - 1 - i]) {
+            resultado = false;
+        }
+    }
+    return resultado;
+}

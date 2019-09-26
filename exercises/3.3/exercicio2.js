1c4725a793af50a5c8df82fe318f00b3a9fb2e71
Guilherme Luiz <guiiluiz44@gmail.com>
2019-09-26 13:52:54 -0300

    cria e resolve exercicio2

---
+++ exercises/3.3/exercicio2.js
@@ -0,0 +1,10 @@
+let array = [2, 3, 6, 7, 10, 1];
+
+let i = 0;
+let indice;
+for (i = 0; i < array.length; i++) {
+    if(array[i] > array[i+1]) {
+        indice = i
+    }
+}
+console.log(indice);

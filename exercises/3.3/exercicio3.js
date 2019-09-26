d1f5fe34f1af8356d74c7eb344ac3f39ef256833
Guilherme Luiz <guiiluiz44@gmail.com>
2019-09-26 16:49:01 -0300

    utiliza function exercicio3

--- exercises/3.3/exercicio3.js
+++ exercises/3.3/exercicio3.js
@@ -1,10 +1,13 @@
-let array = [2, 4, 6, 7, 10, 0, -3];
+function menorNum(array) {
+    let array = [2, 4, 6, 7, 10, 0, -3];
+    
+    let i = 0;
+    let menor = Math.min(...array);
 
-let i = 0;
-let menor = Math.min(...array);
-
-for (i = 0; i < array.length; i++) {
-    if (array[i] == menor){
-        console.log(i);
+    for (i = 0; i < array.length; i++) {
+        if (array[i] == menor) {
+            return i;
+        }
     }
 }
+

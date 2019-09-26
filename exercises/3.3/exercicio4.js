1759d5e06ddbfdf7899c4969f222f011a38ad2c7
Guilherme Luiz <guiiluiz44@gmail.com>
2019-09-26 16:50:23 -0300

    utiliza function exercicio4

--- exercises/3.3/exercicio4.js
+++ exercises/3.3/exercicio4.js
@@ -2,7 +2,6 @@ function maiorNome(array) {
     let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
     let maior = array[0];
-
     for (var i = 0; i < array.length; i++) {
         if (array[i].length > maior.length) {
             maior = array[i];

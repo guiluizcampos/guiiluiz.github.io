02aaaf2d576f162b5f5c40e1cd8e753bf1ae7fa2
Guilherme Luiz <guiiluiz44@gmail.com>
2019-10-15 19:57:06 -0300

    cria funcao do select dos estados

---
+++ exercises/6.1/script.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+    let brazilStates = {
+        'Default': 'Selecione o Estado',
+        'AC': 'Acre',
+        'AL': 'Alagoas',
+        'AP': 'Amapá',
+        'AM': 'Amazonas',
+        'BA': 'Bahia',
+        'CE': 'Ceará',
+        'DF': 'Distrito Federal',
+        'ES': 'Espírito Santo',
+        'GO': 'Goiás',
+        'MA': 'Maranhão',
+        'MT': 'Mato Grosso',
+        'MS': 'Mato Grosso do Sul',
+        'MG': 'Minas Gerais',
+        'PA': 'Pará',
+        'PB': 'Paraíba',
+        'PR': 'Paraná',
+        'PE': 'Pernambuco',
+        'PI': 'Piauí',
+        'RJ': 'Rio de Janeiro',
+        'RN': 'Rio Grande do Norte',
+        'RS': 'Rio Grande do Sul',
+        'RO': 'Rondônia',
+        'RR': 'Roraima',
+        'SC': 'Santa Catarina',
+        'SP': 'São Paulo',
+        'SE': 'Sergipe',
+        'TO': 'Tocantins'
+    }
+
+    for (let stateInitials in brazilStates) {
+        const stateSelect = document.getElementById("select-state");
+        let stateOption = document.createElement('option');
+
+        stateSelect.appendChild(stateOption);
+        stateOption.value = stateInitials;
+        stateOption.innerHTML = brazilStates[stateInitials];
+    }
+    
+}

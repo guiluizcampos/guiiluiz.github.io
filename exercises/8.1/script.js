6f8f030e6407e9771f52cc15c7dc9a0d6911dc7b
Guilherme Luiz <guiiluiz44@gmail.com>
2019-10-28 19:41:08 -0300

    resolve exercicio 2

--- exercises/8.1/script.js
+++ exercises/8.1/script.js
@@ -1,2 +1,19 @@
 // Exercicio 1
-const getFatorial = (number) => {for (let i = number-1; i > 0; i--) number *= i;}
+const getFatorial = (number) => {for (let i = number-1; i > 0; i--) number *= i; console.log(number)}
+getFatorial(4)
+
+// Exercicio 2
+function longestWord(sentence) {
+    const wordsArray = sentence.split(" ");
+    let bigger = '';
+
+    for (let i = 0; i < wordsArray.length; i++) {
+        let characters = wordsArray[i].length
+        if (characters > bigger.length) {
+            bigger = wordsArray[i];
+        }
+    }
+    console.log(bigger)
+}
+
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")

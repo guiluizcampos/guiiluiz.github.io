431ce39547642185c88c8e55d762257cd40cbf53
Guilherme Luiz <guiiluiz44@gmail.com>
2019-11-19 20:04:31 -0300

    cria funcao o proximoPokemon

--- exercises/11.1/my-pokedex/src/App.js
+++ exercises/11.1/my-pokedex/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import Pokedex from './components/Pokedex';
-import pokemons from './data';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Pokedex pokemons={pokemons} />
+      <Pokedex />
     </div>
   );
 }
--- exercises/11.1/my-pokedex/src/components/Pokedex.jsx
+++ exercises/11.1/my-pokedex/src/components/Pokedex.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import data from '../data';
 
 class Pokedex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0
+    }
+  }
+
+  nextPokemon = event => {
+    this.setState({
+      index: this.state.index === data.length-1 ? 0 : this.state.index+=1
+    })
+  }
+
   render() {
     return (
-      <div className="pokedex">
-        {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+      <div className="pokedex-body">
+        <Pokemon key={data[this.state.index].id} pokemon={data[this.state.index]} />
+        <div>
+          <button onClick={this.nextPokemon}>Próximo Pokemon</button>
+        </div>
       </div>
     );
   }

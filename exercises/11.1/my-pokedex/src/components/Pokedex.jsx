e664063cab834989d1b01957da3874cfb64c7504
Guilherme Luiz <guiiluiz44@gmail.com>
2019-11-25 17:02:17 -0300

    cria pokedex com botao para passar pokemon

--- exercises/11.1/my-pokedex/src/components/Pokedex.jsx
+++ exercises/11.1/my-pokedex/src/components/Pokedex.jsx
@@ -6,20 +6,28 @@ class Pokedex extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      index: 0
+      index: 0,
+      type: 'Psychic'
     }
   }
 
   nextPokemon = event => {
+    let newIndex = this.state.index
+    newIndex+=1
     this.setState({
-      index: this.state.index === data.length-1 ? 0 : this.state.index+=1
+      index: this.state.index === data.length-1 ? 0 : newIndex
     })
   }
 
   render() {
+
     return (
       <div className="pokedex-body">
-        <Pokemon key={data[this.state.index].id} pokemon={data[this.state.index]} />
+        <Pokemon pokemon={data[this.state.index]} />
+        <div>
+          <button onClick={this.fireType}>Fire</button>
+          <button onClick={this.psychicType}>Psychic</button>
+        </div>
         <div>
           <button onClick={this.nextPokemon}>Próximo Pokemon</button>
         </div>
